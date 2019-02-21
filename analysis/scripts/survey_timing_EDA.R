738d4ad2013812cb5783030fbd2f2cdecfbb1570
Joel Reynolds <joel_reynolds@nps.gov>
2019-02-20 18:04:41 -0900

    Example of next code chunks to work on in paper.Rmd, and modified survey_timing to clarify what next steps are needed.

--- analysis/scripts/survey_timing_EDA.R
+++ analysis/scripts/survey_timing_EDA.R
@@ -16,6 +16,23 @@ Plot1<- species_data %>%
              linetype=2, colour="grey") +
   labs(x="Julian Date \n (vertical lines denote 1st of April, May, ... and Oct in non-leap years)")
 
+
+#### STILL TO FINISH (then remove "if (0) {","}")
+if (0) {
+  # figure out which dates observed any of the key species - maybe facet on each?
+  # then plot an open circle over that survey date (so we see which surveys were bust)
+  # build toward species specific phenology plots.
+  species_data %>%
+    ggplot(aes(YearDay,Year)) +
+    geom_point(col="blue", size=2) +   # Draw points
+    geom_vline(xintercept=c(yday(ymd(paste("2007-",c(4:10),"-1",sep="")))),
+               linetype=2, colour="grey") +
+    labs(x="Julian Date \n (vertical lines denote 1st of April, May, ... and Oct in non-leap years)")
+  # add new aes & geom_point to plot open circle on just those dates that species X was seen
+  # geom_point....
+}
+
+
 ####### Ideas to pursue later
 #######
 
@@ -31,14 +48,4 @@ if (0) {
 
 }
 
-if (0) {
-# figure out which dates observed any of the key species - maybe facet on each?
-# then plot an open circle over that survey date (so we see which surveys were bust)
-# build toward species specific phenology plots.
-  species_data %>%
-    ggplot(aes(YearDay,Year)) +
-    geom_point(col="blue", size=2) +   # Draw points
-    geom_vline(xintercept=c(yday(ymd(paste("2007-",c(4:10),"-1",sep="")))),
-               linetype=2, colour="grey") +
-    labs(x="Julian Date \n (vertical lines denote 1st of April, May, ... and Oct in non-leap years)")
-}
+
