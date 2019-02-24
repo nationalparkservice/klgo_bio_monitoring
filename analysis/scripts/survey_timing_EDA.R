dd3efdba74630b8545adbdbc95e456cbb227a373
madeleineward <mward2@swarthmore.edu>
2019-02-23 19:07:14 -0500

    Made plot for target species sightings. In progress but runs w/o error.

--- analysis/scripts/survey_timing_EDA.R
+++ analysis/scripts/survey_timing_EDA.R
@@ -11,26 +11,28 @@
 # Display the dates surveys were conducted across each year
 Plot1<- species_data %>%
   ggplot(aes(YearDay,Year)) +
-  geom_point(col="blue", size=2) +   # Draw points
+  geom_point(col="black", size=1.5) +   # Draw points
   geom_vline(xintercept=c(yday(ymd(paste("2007-",c(4:10),"-1",sep="")))),
              linetype=2, colour="grey") +
-  labs(x="Julian Date \n (vertical lines denote 1st of April, May, ... and Oct in non-leap years)")
+  labs(x="Julian Date \n (vertical lines denote 1st of April, May, ... and Oct in non-leap years)") +
+  theme_bw()
 
+species_target_data <- species_data %>%
+  mutate(is_target = ifelse(Common_Name %in% target_data$Common_Name, 1, 0))
 
-#### STILL TO FINISH (then remove "if (0) {","}")
-if (0) {
+#### STILL TO FINISH
   # figure out which dates observed any of the key species - maybe facet on each?
   # then plot an open circle over that survey date (so we see which surveys were bust)
   # build toward species specific phenology plots.
-  species_data %>%
-    ggplot(aes(YearDay,Year)) +
-    geom_point(col="blue", size=2) +   # Draw points
-    geom_vline(xintercept=c(yday(ymd(paste("2007-",c(4:10),"-1",sep="")))),
-               linetype=2, colour="grey") +
-    labs(x="Julian Date \n (vertical lines denote 1st of April, May, ... and Oct in non-leap years)")
-  # add new aes & geom_point to plot open circle on just those dates that species X was seen
-  # geom_point....
-}
+Plot2<- species_target_data %>%
+  ggplot(aes(YearDay,Year)) +
+  geom_point(col="black", size=1.5, aes(shape = as.factor(is_target))) +   # Draw points
+  geom_vline(xintercept=c(yday(ymd(paste("2007-",c(4:10),"-1",sep="")))),
+             linetype=2, colour="grey") +
+  labs(x="Julian Date \n (vertical lines denote 1st of April, May, ... and Oct in non-leap years)")+
+  scale_shape_manual(values = c(16, 21)) +
+  theme_bw() +
+  facet_wrap(~Common_Name)
 
 
 ####### Ideas to pursue later
