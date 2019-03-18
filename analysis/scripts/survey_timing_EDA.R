76e84b05d645f22fafc7967f93593648139c733c
madeleineward <mward2@swarthmore.edu>
2019-03-18 07:48:57 -0400

    Tried to use a join to get a set with all dates for all species, then an indicator of whether each was spotted on that day. Probably needs a separate set for each species?

--- analysis/scripts/survey_timing_EDA.R
+++ analysis/scripts/survey_timing_EDA.R
@@ -5,11 +5,11 @@
 # Libraries used: ggplot2,
 ########
 # Joel Reynolds (repurposing code by Madeleine Ward)
-# Last Edit: 2019 Feb 8
+# Last Edit: 2019 Mar 17
 ########
 
 # Display the dates surveys were conducted across each year
-Plot1<- species_data %>%
+plot_surveydates <- species_data %>%
   ggplot(aes(YearDay,Year)) +
   geom_point(col="black", size=1.5) +   # Draw points
   geom_vline(xintercept=c(yday(ymd(paste("2007-",c(4:10),"-1",sep="")))),
@@ -17,21 +17,36 @@ Plot1<- species_data %>%
   labs(x="Julian Date \n (vertical lines denote 1st of April, May, ... and Oct in non-leap years)") +
   theme_bw()
 
-species_target_data <- species_data %>%
-  mutate(is_target = ifelse(Common_Name %in% target_data$Common_Name, 1, 0))
+#UNFINISHED
+all_dates <- species_data %>%
+  select(Date) %>%
+  unique() %>%
+  mutate(all_date = 1)
+
+all_targets <- target_data %>%
+  mutate(sighting_date = ifelse(Date %in% all_dates$Date, 1, 0))
+
+target_dates <- full_join(all_dates, all_targets, by = "Date")
+
+full_join(all_dates, by = "Date")
 
 #### STILL TO FINISH
-  # figure out which dates observed any of the key species - maybe facet on each?
-  # then plot an open circle over that survey date (so we see which surveys were bust)
-  # build toward species specific phenology plots.
-Plot2<- species_target_data %>%
-  ggplot(aes(YearDay,Year)) +
-  geom_point(col="black", size=1.5, aes(shape = as.factor(is_target))) +   # Draw points
-  geom_vline(xintercept=c(yday(ymd(paste("2007-",c(4:10),"-1",sep="")))),
-             linetype=2, colour="grey") +
+# figure out which dates observed any of the key species - maybe facet on each?
+# then plot an open circle over that survey date (so we see which surveys were bust)
+# build toward species specific phenology plots.
+
+
+Plot2<- ggplot(target_dates, aes(YearDay, Year)) +
+  geom_point(shape = 21)
+
+Plot3 <- Plot2 +
+  geom_point(aes(shape = as.factor(is_sighted))) +
+  facet_wrap(~Common_Name)
+
+geom_vline(xintercept=c(yday(ymd(paste("2007-",c(4:10),"-1",sep="")))),
+           linetype=2, colour="grey") +
   labs(x="Julian Date \n (vertical lines denote 1st of April, May, ... and Oct in non-leap years)")+
   scale_shape_manual(values = c(16, 21)) +
-  theme_bw() +
   facet_wrap(~Common_Name)
 
 
@@ -47,7 +62,6 @@ if (0) {
     geom_vline(xintercept=c(yday(ymd(paste("2007-",c(4:10),"-1",sep="")))),
                linetype=2, colour="grey") +
     labs(x="Julian Date \n (vertical lines denote 1st of April, May, ... and Oct in non-leap years)")
-
+  
 }
-
 
