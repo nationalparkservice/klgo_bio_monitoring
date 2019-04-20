68a12a4bc000869aafe8ea468c1759c5fda3394a
madeleineward <mward2@swarthmore.edu>
2019-04-20 13:43:22 -0400

    some changes to explore possibilities for species_occurrence graphs

--- analysis/scripts/occurrence_freq.R
+++ analysis/scripts/occurrence_freq.R
@@ -7,10 +7,20 @@
 # Last Edit: 2019 Apr 16
 ########
 
+all_surveys <- species_data %>%
+  select(Date, Year, Month, Day, YearDay, Duration) %>%
+  distinct(Date
+           #, .keep_all = TRUE
+           )
 
 occurrence_data <- target_data %>%
-  group_by(Year) #%>%
-  #mutate()
+  filter(Species == "MEGU")
+left_join(all_surveys, occurrence_data, by = "Date")
+#%>%
+#mutate()
+
+
+
 
 #second graph shows observations each year relative to other species
 #percent_obs will be the percent of observations each date that 
@@ -29,4 +39,15 @@ ggplot(occurrence_species, aes(Year, percent_obs)) +
   geom_boxplot(aes(group = Year)) +
   #for now, limit set at 50% - how to account for variability?
   scale_y_continuous(limits = c(0, .50)) +
-  facet_wrap(~Species)
+  coord_flip() +
+  facet_wrap(~Common_Name)
+
+
+occurrence_species2 <- target_data %>%
+  group_by(Year) %>%
+  mutate(all_obs = sum(obs_date)) %>%
+  ungroup() %>%
+  #find percent of observations accreditable to each species
+  group_by(Species, Year) %>%
+  mutate(percent_obs = obs_date/all_obs)
+  
