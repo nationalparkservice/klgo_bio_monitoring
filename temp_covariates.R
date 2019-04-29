e37b4fad4cd3f475e935b5ac94ae91d73c95ee84
madeleineward <mward2@swarthmore.edu>
2019-04-29 17:27:40 -0400

    forgot to commit these changes: creation of distribution script

---
+++ temp_covariates.R
@@ -0,0 +1,16 @@
+############
+# Examine various covariates and distributions.
+########
+# Libraries used: 
+########
+# Madeleine Ward
+# Last Edit: 2019 Apr 27
+########
+
+# Distribution of weather conditions
+#plot_winddist <- ggplot(tbl_Events, aes(as.factor(Year))) +
+ # geom_bar(fill = as.factor(Wind_code))
+
+
+#plot_winddist
+
