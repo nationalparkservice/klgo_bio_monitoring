996d61bc8c31b1f6f33134998350db388cfee258
madeleineward <mward2@swarthmore.edu>
2019-05-01 13:27:06 -0400

    Finish up survey_dates

--- analysis/scripts/survey_dates.R
+++ analysis/scripts/survey_dates.R
@@ -3,32 +3,22 @@
 # need to be modified to account for any observed changes in phenology
 # of the target species.
 ########
-# Libraries used: dplyr, ggplot2
+# Libraries used: dplyr, ggplot2, ggforce
 ########
 # Madeleine Ward
 # Last Edit: 2019 Mar 17
 ########
 
+# collect all survey dates and arrange by timing
 firstlast_surveys <- species_data %>%
+  select(Year, YearDay) %>%
   group_by(Year) %>%
   arrange(YearDay) 
 
-first_surveys <- firstlast_surveys %>%
-  slice(1) %>%
-  select(Year, YearDay)
-last_surveys <- firstlast_surveys %>%
-  slice(n()) %>%
-  select(Year, YearDay)
-
-
 #dataset with both start and end dates for each target species
 target_start_end <- target_data %>%
-  mutate(is_first = ifelse(Date %in% first_surveys$Date, 1, 0)) %>%
-  # now this will find first/last days
-  # group by year
-  group_by(Common_Name, Year
-           #         , Location_ID
-  ) %>%
+  # find first/last days, by year
+  group_by(Common_Name, Year) %>%
   arrange(YearDay) %>%
   # take first and last day for each group
   slice(c(1, n())) %>%
@@ -36,56 +26,45 @@ target_start_end <- target_data %>%
   mutate(Start_End = ifelse(row_number()==1, "Start", "End")) %>%
   ungroup() 
 
-# this plot shows start dates of each survey, as well as
+## this plot shows start dates of each survey, as well as
 # first sighting of each target species
-
-# used species HERG as placeholder until I can get facet to work
 plot_timing_1 <- ggplot() +
+  # select all species-specific start dates
   geom_point(data =target_start_end %>%
                filter(Start_End == "Start"
                       #, Species == "HERG"
                ), 
              aes(x=Year, y=YearDay), color="red") +
-  geom_line(data =first_surveys
-            #%>%
-            #            filter(is_first == 1
-            #                   )
-            , 
+  # select all survey start dates
+  geom_line(data =firstlast_surveys %>%
+              slice(1), 
             aes(x=Year, y=YearDay), color="black") +
-  #geom_line(data =first_surveys 
-            #%>%
-  #            filter(is_first == 1
-  #                   )
-  #, 
-   #         aes(x=Year, y=YearDay, color="blue")) +
+  # titles and Julian date labels
 labs(title = "Date of First Observation by Year", y = "Julian Date \n 
        (vertical lines denote 1st of April, May, ... 
      and Oct in non-leap years)")  +
-#scale_color_discrete(values = c("First Day of Survey Season" = "black")) + 
   geom_hline(yintercept=c(yday(ymd(paste("2007-",c(4:6),"-1",sep="")))),
              linetype=2, colour="grey") + 
-  theme(legend.position="bottom") +
-  facet_wrap(~Common_Name)
-             #, scales = "free_y"
+  # facet by species; try to separate into visible columns
+  facet_wrap_paginate(~Common_Name, ncol = 3
+             #, scales = "free_y")
+  )
 
+## last sighting of each species
 plot_timing_2 <- ggplot() +
+  # select all species-specific end dates
   geom_point(data =target_start_end %>%
-               filter(Start_End == "End"
-                      #, Species == "HERG"
-               ), 
+               filter(Start_End == "End"), 
              aes(x=Year, y=YearDay), color="blue") +
-  geom_line(data =last_surveys
-            #%>%
-            #            filter(is_first == 1
-            #                   )
-            , 
+  # select all survey end dates
+  geom_line(data =firstlast_surveys %>%
+              slice(n()), 
             aes(x=Year, y=YearDay), color="black") +
+  # titles and Julian date labels
   labs(title = "Date of Last Observation by Year", y = "Julian Date \n 
        (vertical lines denote 1st of April, May, ... 
        and Oct in non-leap years)")  +
   #scale_color_discrete(values = c("First Day of Survey Season" = "black")) + 
   geom_hline(yintercept=c(yday(ymd(paste("2007-",c(4:6),"-1",sep="")))),
              linetype=2, colour="grey") + 
-  theme(legend.position="bottom") +
-  facet_wrap(~Common_Name, ncol = 3)
-
+  facet_wrap_paginate(~Common_Name, ncol = 3)
