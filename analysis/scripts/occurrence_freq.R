a3772e6eb1dc643365f6beb8c9879e97dffeaf84
madeleineward <mward2@swarthmore.edu>
2019-05-04 19:59:03 -0400

    finished occurrence plots; will later add to paper.rmd

--- analysis/scripts/occurrence_freq.R
+++ analysis/scripts/occurrence_freq.R
@@ -7,22 +7,7 @@
 # Last Edit: 2019 Apr 16
 ########
 
-all_surveys <- species_data %>%
-  select(Date, Year, Month, Day, YearDay, Duration) %>%
-  distinct(Date
-           #, .keep_all = TRUE
-           )
-
-occurrence_data <- target_data %>%
-  filter(Species == "MEGU")
-left_join(all_surveys, occurrence_data, by = "Date")
-#%>%
-#mutate()
-
-
-
-
-#second graph shows observations each year relative to other species
+# graph shows observations each year relative to other TARGET species
 #percent_obs will be the percent of observations each date that 
 #the spp composed
 occurrence_species <- target_data %>%
@@ -31,18 +16,10 @@ occurrence_species <- target_data %>%
   mutate(all_obs = sum(obs_date)) %>%
   ungroup() %>%
   #find percent of observations accreditable to each species
-  group_by(Species, Year, YearDay) %>%
-  mutate(percent_obs = obs_date/all_obs) #%>%
-  #filter(Species == "HERG")
-
-ggplot(occurrence_species, aes(Year, percent_obs)) +
-  geom_boxplot(aes(group = Year)) +
-  #for now, limit set at 50% - how to account for variability?
-  scale_y_continuous(limits = c(0, .50)) +
-  coord_flip() +
-  facet_wrap(~Common_Name)
-
-
+  group_by(Year, YearDay, Species) %>%
+  mutate(percent_obs = obs_date/all_obs) %>%
+  ungroup() 
+#each year
 occurrence_species2 <- target_data %>%
   group_by(Year) %>%
   mutate(all_obs = sum(obs_date)) %>%
@@ -50,4 +27,12 @@ occurrence_species2 <- target_data %>%
   #find percent of observations accreditable to each species
   group_by(Species, Year) %>%
   mutate(percent_obs = obs_date/all_obs)
-  
+
+occurrence_species$Common_Name <- reorder(occurrence_species$Common_Name, -occurrence_species$percent_obs)
+
+plot_occurrence <- ggplot(occurrence_species, aes(YearDay, percent_obs)) +
+  geom_point(alpha = .1) +
+  geom_line(alpha = .02) +
+  theme_bw() +
+  facet_wrap(~Common_Name, nrow = 5)
+plot_occurrence
